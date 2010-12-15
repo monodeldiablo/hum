f975e82b170e51ce123f20b0496ffea186417de8
Javier Jardón <jjardon@gnome.org>
2010-12-15 15:38:48 +0000

    Use autoreconf instead gnome-autogen

--- autogen.sh
+++ autogen.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
-srcdir=`dirname $0`
-test -z "$srcdir" && srcdir=.
-
-PKG_NAME="hum"
-
-. gnome-autogen.sh
+test -n "$srcdir" || srcdir=`dirname "$0"`
+test -n "$srcdir" || srcdir=.
+(
+  cd "$srcdir" &&
+  autopoint --force &&
+  AUTOPOINT='intltoolize --automake --copy' autoreconf --force --install
+) || exit
+test -n "$NOCONFIGURE" || "$srcdir/configure" "$@"
